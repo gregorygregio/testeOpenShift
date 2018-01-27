1b8f791011f86f2c52936c97dd526ece17552b4f
gregorygregio <gregorygregio@hotmail.com>
2018-01-27 21:03:34 -0200

    aa

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const moduloProduto = require('./src/controllers/produtoController');
-const moduloVenda = require('./src/controllers/vendasController');
+// const moduloProduto = require('./src/controllers/produtoController');
+// const moduloVenda = require('./src/controllers/vendasController');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended:false} ))
 
+app.get("/teste", (req,res) => {
+  res.send("Funfando")
+})
 
-moduloProduto(app);
-moduloVenda(app);
 
-app.listen(3000);
+// moduloProduto(app);
+// moduloVenda(app);
+
+app.listen(8080);
