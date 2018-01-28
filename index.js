f97a7c12e944bf9bf1fede388610e4ce19b248a0
gregorygregio <gregorygregio@hotmail.com>
2018-01-27 22:27:28 -0200

    aa

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/teste", (req,res) => {
 // moduloVenda(app);
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
